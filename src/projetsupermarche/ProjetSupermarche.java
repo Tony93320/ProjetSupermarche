6abf202601c487e0f9199da38a25cadca59ca2e9
Tony93320 <antho93kbs@gmail.com>
2022-04-11 11:51:52 +0200

    Création du menu principal

--- src/projetsupermarche/ProjetSupermarche.java
+++ src/projetsupermarche/ProjetSupermarche.java
@@ -5,6 +5,8 @@
  */
 package projetsupermarche;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ANTHONY
@@ -16,6 +18,51 @@ public class ProjetSupermarche {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        
+        
+        
+        
+        
+                Scanner sc = new Scanner(System.in);
+        int choix = 0;
+        do
+        {
+            do
+            {
+                System.out.println("1 : Afficher toutes les informations");
+                System.out.println("2 : Temps total pour un employé");
+                System.out.println("3 : Temps moyen travaillé par l'ensemble des employés");
+                System.out.println("0 : Quitter le programme");
+                choix = sc.nextInt();
+
+            }while(choix < 0 || choix > 3);
+
+            switch(choix)
+            {
+                case 1 : 
+                     // A vous de jouer
+                    
+
+                    break;
+                case 2 :
+                    
+                    // A vous de jouer
+                    
+                    
+                    break;
+                case 3 : 
+
+                    // A vous de jouer
+                    
+                    
+                    break;    
+                case 0 : 
+                    System.out.println("FIN DU PROGRAME");
+                    break;
+            }
+        }while(choix!=0);
     }
     
 }
